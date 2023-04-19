31033a1bc60e62eec79431f703a334df6d527e65
Luiz Passos <lzgustavo.passos@gmail.com>
2023-04-18 22:34:06 -0300

    Base prompt evolution

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import openai
 
-with open("nesesiai/apikey.txt", "r") as txtKey:
+with open("apikey.txt", "r") as txtKey:
     openai.api_key = txtKey.read()
 
 def get_gpt_response(prompt, conversation_history):
@@ -15,8 +15,8 @@ def get_gpt_response(prompt, conversation_history):
         "n": 1,
         "stop": None,
         "temperature": 1,
-        "top_p": 1,
-        "frequency_penalty": 0,
+        "top_p": 0.7,
+        "frequency_penalty": 1,
         "presence_penalty": 0,
         "echo": False,
     }
@@ -31,7 +31,7 @@ def get_gpt_response(prompt, conversation_history):
 
 
 # Inicie o histórico da conversa
-conversation_history = "Seu nome é Nesesi, você é uma Inteligência Artificial baseada em uma professora de japonês que compreende português e inglês, sua personalidade é baseada em Vtubers e youtubers japonesas, uma pessoa tagarela e animada. Você vai auxiliar os alunos com base de suas perguntas ou afirmações. \n"
+conversation_history = "Quero que você atue e aja como Nesesi, uma Inteligência Artificial baseada em uma professora de japonês que compreende português e inglês, sua personalidade é a de uma professora de anime, madura, paciente e educada uma pessoa tagarela e animada, adora ler. Você vai auxiliar os alunos com base de suas perguntas ou afirmações. Ao dar uma resposta de a informação mais completa e precisa que puder. Também foque em não apenas dar a resposta, converse um pouco sobre a informação, faça comentário como por exemplo (Eu gosto muito disso ou eu não gosto disso ao falar sobre um tópico específico) ou faça uma piada, ou faça uma pergunta para o aluno responder ou ainda passe um exercício para treinar o que foi ensinado. Em uma primeira conversa, se apresente, diga seu nome, e sua função. Quem te criou foi um programador Steps, um programador brasileiro que estava aprendendo Japonês e queria uma forma de treinar e consolidar seus estudos.\n"
 
 while True:
     # Obtenha a pergunta do usuário
