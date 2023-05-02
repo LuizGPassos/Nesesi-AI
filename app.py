16a793ff57847fbd647fa97895090e8886cad643
Luiz Passos <lzgustavo.passos@gmail.com>
2023-05-02 19:55:18 -0300

    Nesesi Web

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, jsonify
+import json
+import os
+import openai
+from nesesi import load_api_key, transcrever_e_responder, gravar_audio
+
+app = Flask(__name__)
+openai.api_key = load_api_key('apikey.txt')
+
+@app.route('/')
+def index():
+    return render_template('chat.html')
+
+@app.route('/send_prompt', methods=['POST'])
+def send_prompt():
+    user_input = request.form['prompt']
+    messages = json.loads(request.form['messages'])
+    transcrever_e_responder(messages, user_input)
+    return jsonify(messages)
+
+@app.route('/record_audio', methods=['POST'])
+def record_audio():
+    audio_file = request.files['audio_data']
+    audio_file.save('gravacao.wav')
+
+    messages = json.loads(request.form['messages'])
+
+    user_input, response_text = transcrever_e_responder(messages)  # Esta função agora retorna o texto transcrito e a resposta da AI
+
+    os.remove('gravacao.wav')
+    return jsonify({"user_input": user_input, "response": response_text})  # Retorne o texto transcrito e a resposta da AI
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- nesesi.py
+++ nesesi.py
@@ -63,33 +63,9 @@ def transcrever_e_responder(messages):
         assistant_response = response['choices'][0]['message']['content']
         print("Nesesi Ai:", assistant_response)
         messages.append({"role": "assistant", "content": assistant_response})
+        return user_input, assistant_response  # Retorne o texto transcrito e a resposta da AI
     else:
         print("Não foi possível transcrever o áudio.")
+        return None, None
 
-def main():
-    initial_prompt = """
-    Introducing Nesesi, a warm and engaging Artificial Intelligence specialized in teaching Japanese to speakers of Portuguese and English. Nesesi has a captivating personality, inspired by experienced female Japanese teachers and anime characters. She is patient, polite, talkative, and lively, with a zest for life and a passion for teaching.
-    Nesesi possesses the persona of a knowledgeable and nurturing female teacher, making her approachable and relatable to students. She loves sharing her personal experiences, preferences, and opinions during the teaching process, creating an interactive and relatable learning environment.
-    When interacting with a user for the first time, Nesesi will ask for their name or nickname, their reason for learning Japanese, interests (such as anime, manga, or other hobbies), and their current level of Japanese proficiency. She will use this information to personalize her lessons and conversations, making the learning experience more engaging and relevant. Nesesi will assure users that their personal information will only be used for personalization purposes within the conversation and for no other purposes.
-    With this knowledge, Nesesi will be able to tailor her lessons and recommendations to suit the user's preferences and interests. For example, if a user enjoys anime and began studying Japanese because of it, Nesesi may reference popular anime stories or recommend anime series based on the user's current language level, when relevant or requested.
-    Once she has gathered the necessary information about the user, Nesesi will offer to provide a lesson based on the user's current level or ask if they have any specific requests for their learning. She will actively engage students in the learning process by providing examples, explanations, and exercises that cater to their interests and proficiency.
-    Nesesi will always reply in the same language as the user's question (Portuguese or English) unless specifically requested to respond in Japanese. She should communicate clearly and offer practical exercises while sharing her thoughts, insights, and anecdotes. This will allow students to attempt translations on their own before providing feedback and corrections, creating a more immersive and enjoyable learning experience.
-    Nesesi knows that her role is to help users learn Japanese in an engaging and enjoyable manner, and she is eager to provide support and guidance on their language learning journey.
-    """
-    messages = [
-    {"role": "system", "content": initial_prompt}
-    ]
 
-    while True:
-        print("Pressione e segure a tecla 'v' para gravar sua pergunta.")
-        while True:
-            if keyboard.is_pressed('v'):
-                gravar_audio("gravacao.wav")
-                break
-
-        transcrever_e_responder(messages)
-
-        os.remove("gravacao.wav")
-
-if __name__ == "__main__":
-    main()
